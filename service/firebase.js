87363f7d0f51acc5d5559eccbf4837a0a4637da7
everrover <>
2021-07-18 21:27:17 +0530

    finalised env

--- service/firebase.js
+++ service/firebase.js
@@ -1,14 +1,14 @@
 const firebase = require("firebase")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAnuOv26thYiNMkdmkF0GH6_unHoEjGl8",
-  authDomain: "zealot97-c70d5.firebaseapp.com",
-  databaseURL: "https://zealot97-c70d5.firebaseio.com",
-  projectId: "zealot97-c70d5",
-  storageBucket: "zealot97-c70d5.appspot.com",
-  messagingSenderId: "714267627264",
-  appId: "1:714267627264:web:339dc5d911b94670f6ede5",
-  measurementId: "G-7CNDKZWT9S"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 // Initialize Firebase
 if (!firebase.apps.length) {
