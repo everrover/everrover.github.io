8e65644d619575cfb94f14d9a8de737a1030819a
everrover <>
2021-07-18 09:47:38 +0530

    made config changes

--- components/LogoImg.jsx
+++ components/LogoImg.jsx
@@ -1,12 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import Image from 'next/image'
-// import Logo from '/logo.svg';
 
 export default function Logo(props) {
   const {size="h-20 w-20"} = props
   return (
     <div className={`${size} m-auto text-center mr-2`}>
-      <Image src="/logo.svg" alt="logo" title="Everrover" height="64" width="64" layout="responsive" loading="eager"/>
+      <img src="/logo.svg" alt="logo" title="Everrover" layout="responsive" loading="eager"/>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ module.exports = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['firebasestorage.googleapis.com', 'images.pexels.com', "miro.medium.com", "mouapp.com", "smallerapp.com", "resizesafari.com"],
+    domains: ['firebasestorage.googleapis.com'],
+    loader: 'imgix', // this is a hack until the bug is fixed
+    path: ''
   },
   async redirects() {
     return [
