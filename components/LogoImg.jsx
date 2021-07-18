e9f9964d4c48d62ab0e6087cc426a212566693f3
everrover <>
2021-07-18 09:53:41 +0530

    sth

--- components/LogoImg.jsx
+++ components/LogoImg.jsx
@@ -5,7 +5,7 @@ export default function Logo(props) {
   const {size="h-20 w-20"} = props
   return (
     <div className={`${size} m-auto text-center mr-2`}>
-      <img src="/logo.svg" alt="logo" title="Everrover" layout="responsive" loading="eager"/>
+      <img src="/images/logo.svg" alt="logo" title="Everrover" layout="responsive" loading="eager"/>
     </div>
   )
 }
